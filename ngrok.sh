4cd848517d2179e84f59dea54e88ac15feab6e9b
ChrisCruze <cruzc09@gmail.com>
2020-12-18 20:56:57 -0500

    updated

--- ngrok.sh
+++ ngrok.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./ngrok authtoken 1llddKdRMILR2uEZtYUvvo0Sf4H_5ecWLSS84wbWfdnre45uj
-./ngrok tcp 22
+/home/triton/UbuntuServer/ngrok authtoken 1llddKdRMILR2uEZtYUvvo0Sf4H_5ecWLSS84wbWfdnre45uj
+/home/triton/UbuntuServer/ngrok tcp 22
